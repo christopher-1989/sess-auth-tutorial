6ea32c73740c750fc50c2664c1d79eb8a165d25c
Christopher McDonald <c.robin.mcdonald@gmail.com>
2021-07-21 18:57:09 +1000

    Finished project with basic HTML and list of TODOs for further practice

--- app.js
+++ app.js
@@ -1,10 +1,164 @@
 const express = require('express');
 const session = require('express-session');
+const bodyParser = require('body-parser')
+
+const TWO_HOURS = 1000 * 60 * 60 * 2;
 
 const {
-    PORT = 3000
-} = process.env
+    PORT = 3000,
+    NODE_ENV = 'development',
+    SESS_NAME = 'sid',
+    SESS_SECRET = 'this is a test//secret!', //TODO: strengthen secret in prod
+    SESSION_LIFETIME = TWO_HOURS,
+} = process.env;
+
+const IN_PROD = NODE_ENV === 'production';
+
+const users = [ //TODO: DB
+    {id: 1, name: 'Alex', email: 'alex@gmail.com', password: 'secret1'},
+    {id: 2, name: 'Bob', email: 'bob@gmail.com', password: 'secret2'},
+    {id: 3, name: 'Cat', email: 'cat@gmail.com', password: 'secret3'},
+]
 
 const app = express();
 
-app.listen(PORT);
+app.use(bodyParser.urlencoded({
+    extended: true
+}))
+
+app.use(session({
+    name: SESS_NAME,
+    resave: false,
+    saveUninitialized: false,
+    secret: SESS_SECRET,
+    cookie: {
+        maxAge: SESSION_LIFETIME,
+        sameSite: true, //strict
+        secure: IN_PROD,
+    }
+}))
+
+const redirectLogin = (req, res, next) => {
+    if (!req.session.userId) {
+        res.redirect('/login')
+    } else {
+        next();
+    }
+}
+
+const redirectHome = (req, res, next) => {
+    if (req.session.userId) {
+        res.redirect('/home')
+    } else {
+        next();
+    }
+}
+
+app.use((req, res, next) => {
+    const { userId } = req.session;
+    if (userId) {
+        res.locals.user = users.find(
+            user => user.id === userId
+        )
+    }
+    next();
+})
+
+app.get('/', (req, res) => {
+const { userId } = req.session;
+console.log(userId)
+    res.send(`
+
+        ${userId ? `
+        <h1>Welcome!</h1>
+        <a href='/home'>Home</a>
+        <form method='post' action='/logout'>
+            <button>Logout</button>
+        </form>
+        `:`
+        <a href='/login'>Login</a>
+        <a href='/register'>Register</a>
+        `}
+        `)
+})
+app.get('/home', redirectLogin, (req, res) => {
+    const { user } = res.locals;
+    console.log(req.sessionID)
+    res.send(`
+    <h1>Home</h1>
+    <a href='/'>Main</a>
+    <ul>
+        <li>Name: ${user.name}</li>
+        <li>Email: ${user.email}</li>
+    `)
+})
+app.get('/login', redirectHome, (req, res) => {
+    res.send(`
+        <h1>Login</h1>
+        <form method='post' action='/login'>
+            <input type='email' name='email' placeholder='Email' required />
+            <input type='password' name='password' placeholder='Password' required />
+            <input type='submit' />
+        </form>
+        <a href='/register'>Register</a>
+        `)
+})
+
+app.get('/register', redirectHome, (req, res) => {
+    res.send(`
+    <h1>Register</h1>
+    <form method='post' action='/register'>
+        <input name='name' placeholder='Name' required />
+        <input type='email' name='email' placeholder='Email' required />
+        <input type='password' name='password' placeholder='Password' required />
+        <input type='submit' />
+    </form>
+    <a href='/login'>Login</a>
+    `)
+})
+app.post('/login', redirectHome, (req, res) => {
+    const { email, password } = req.body;
+
+    if (email && password) { //TODO: validation
+        const user = users.find(user => user.email === email && user.password === password) //TODO: hash
+
+        if (user) {
+            req.session.userId = user.id;
+            return res.redirect('/home');
+        }
+    }
+    res.redirect('/login');
+})
+app.post('/register', redirectHome, (req, res) => {
+    const { name, email, password } = req.body;
+
+    if(name && email && password) { //TODO: validation
+        const exists = users.some(
+            user => user.email === email
+        )
+        if (!exists) {
+            const user = {
+                id: users.length + 1,
+                name, 
+                email,
+                password //TODO: hash
+            }
+        users.push(user);
+        req.session.userId = user.id;
+        return res.redirect('/home');
+        }
+    }
+    res.redirect('/register'); //TODO: qs errors for example /register?error=error.auth.userExists
+})
+app.post('/logout', redirectLogin, (req, res) => {
+    req.session.destroy(err => {
+        if (err) {
+            return res.redirect('/home')
+        }
+        res.clearCookie(SESS_NAME);
+        res.redirect('/login');
+    })
+})
+app.listen(PORT, () => console.log(
+    `http://localhost:${PORT}`
+));
